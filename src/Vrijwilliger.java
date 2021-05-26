57b843087c5d48ba9efbf9a9c661bc46c50dcc01
ARMANDOV369 <armandovelders@gmail.com>
2021-05-26 19:42:36 +0200

    .gitignore

---
+++ src/Vrijwilliger.java
@@ -0,0 +1,25 @@
+//public class Vrijwilliger {
+//    String naam;
+//    Integer leeftijd;
+//    Boolean ZZP;
+//    Double v = 0.0;
+//
+//    public Vrijwilliger(String naam, Integer leeftijd, Boolean ZZP){
+//        this.naam = naam;
+//        this.leeftijd = leeftijd;
+//        this.ZZP = ZZP;
+//    }
+//
+//    public Double Vergoeding(Integer uren){
+//        if (Vrijwilliger.this.v < 180.00) {
+//            if (this.leeftijd > 21) { v = uren*5.00; }
+//            else {
+//                if (this.ZZP) { v = uren*35.00; }
+//                else { v = uren*2.50; } } }
+//        return v;
+//    }
+//}
+//
+//
+//
+//
